8ac423eced7adc2afbb69be0bca10f3c99f8d1a8
psegarel <patrick@ps.dev>
2012-12-27 17:07:22 +0800

    Switched to frameboot theme & the jQuery Multi module

---
+++ sites/all/themes/frameboot/templates/custompages/awards.tpl.php
@@ -0,0 +1,16 @@
+<div class="container">
+	<div class="row-fluid">
+		<div class="span6">								
+			<div id="awards" class="pull-right">
+				2012 - "Top Magazine" Award for coaching/teaching blind Chinese girl pianist , Niko<br/>
+				2012 - "Top Magazine" Cultural Humanitarian Award<br />
+				2011 - China Art Agency - Best Jazz Artist Award<br />
+				2011 - China Art - Best Pop Opera Artist Award<br />
+				2010 - Beijing Government - Humanitarian Award<br/>
+				2009 - Guangzhou Artist Award<br />
+				2004 - Mongolian Award for Artistic Education in Mongolia<br />
+				1999 - Russia Awards for concerts across Siberia
+			</div>
+		</div>
+	</div>
+</div>
---
+++ sites/all/themes/frameboot/templates/custompages/gallery.tpl copy.php
@@ -0,0 +1,16 @@
+<div class="row-fluid">
+	<div class="span12">
+		<?php  print custompage_view_tile( 'gallery', $title=FALSE , $display_id = 'gallery');?>
+	</div>
+</div>
+<div class="row-fluid">
+	<div class="span12">
+		<?php  print custompage_view_tile( 'gallery', $title=FALSE , $display_id = 'gallery_row_2');?>
+	</div>
+</div>
+<div class="row-fluid">
+	<div class="span12">
+		<?php  print custompage_view_tile( 'gallery', $title=FALSE , $display_id = 'gallery_row_3');?>
+	</div>
+</div>
+
