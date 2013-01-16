eee592c76ce95ec1db2b5086b3c0d3924af48929
psegarel <patrick@ps.dev>
2013-01-17 00:51:24 +0800

    Created Omega branch, switching to Omega Theme

---
+++ sites/all/themes/omegabob/templates/custompages/gallery.tpl.php
@@ -0,0 +1,17 @@
+<div class="row-fluid">
+	<div class="span12">
+		<?php  print views_embed_view( 'gallery', $display_id = 'gallery'); ?>
+	</div>
+</div>
+<div class="row-fluid">
+	<div class="span12">
+		<?php  print views_embed_view( 'gallery', $display_id = 'gallery_row_2'); ?>
+	</div>
+</div>
+<div class="row-fluid">
+	<div class="span12">
+		<?php  print views_embed_view( 'gallery', $display_id = 'gallery_row_3'); ?>
+	</div>
+</div>
+
+
